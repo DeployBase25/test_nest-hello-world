a00d9ea17440233a111ceb544cf9dcc3663f4214
DeployBase25 <deploybase@proton.me>
2025-12-30 22:34:11 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Huuuray!!!';
+    return 'Hello! This is a backend node.js/nestjs framework server, running inside DeployBase servers, and is deployed from github repository where this code is originated from as a testing backend app';
   }
 
   getPing(): string {
-    return 'Pong!';
+    return 'Hi from the next path/route!';
   }
 }
